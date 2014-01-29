fb5ada67b4a036825de8efc2f7ae72a2b7464e7b
Frank Bergmann <frank.bergmann@project-open.com>
2014-01-29 16:06:43 +0000

    - changed report label

--- www/reports/unsubmitted-hours.tcl
+++ www/reports/unsubmitted-hours.tcl
@@ -31,7 +31,7 @@ ad_page_contract {
 # Label: Provides the security context for this report
 # because it identifies unquely the report's Menu and
 # its permissions.
-set menu_label "reporting-unsubmitted-hours"
+set menu_label "reporting-timesheet-unsubmitted-hours"
 set current_user_id [ad_maybe_redirect_for_registration]
 set read_p [db_string report_perms "
 	select	im_object_permission_p(m.menu_id, :current_user_id, 'read')
